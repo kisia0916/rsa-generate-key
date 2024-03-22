6627f12da097485955a97ce8593a69292152fce3
fumi <93899734+kisia0916@users.noreply.github.com>
2024-03-22 14:38:14 +0900

    test

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 
-use std::fmt::format;
-
+use std::{collections::btree_map::Range, f32::consts::E, fmt::format};
+use num_bigint::{BigUint,ToBigInt};
+use num_traits::{pow, Pow};
 use rand::Rng;
 struct rand_range{
     p1s:i32,
@@ -41,13 +42,13 @@ fn generate_prime_num(max:usize)->i32{
 fn genarate_random_prime_num()->(i32,i32){
     let mut rand = rand::thread_rng();
     let mut p_rand_range = rand_range{
-        p1s:10000,
-        p1l:rand.gen_range(10001..50000),
-        p2s:10000,
-        p2l:rand.gen_range(10001..50000)
+        p1s:10,
+        p1l:rand.gen_range(11..80),
+        p2s:10,
+        p2l:rand.gen_range(11..80)
     };
     while p_rand_range.p1l == p_rand_range.p2l {
-        p_rand_range.p2l = rand.gen_range(10001..50000);
+        p_rand_range.p2l = rand.gen_range(11..80);
     }
     let p1_rand:i32 = rand.gen_range(p_rand_range.p1s..p_rand_range.p1l);
     let p2_rand:i32 = rand.gen_range(p_rand_range.p2s..p_rand_range.p2l);
@@ -71,23 +72,31 @@ fn generate_public_key(p1:i32,p2:i32)->public_key{
 fn generate_private_key(p1:i32,p2:i32,e:i32)->private_key{
     let mut rand = rand::thread_rng();
     let n = ((p1 - 1)*(p2 - 1)) as u64;
-    let d= (rand.gen_range(100..1000)*n+1)/(e as u64);
+    let d= (rand.gen_range(2..3)*n+1)/(e as u64);
     private_key{d}
 }
-fn find_mod(n: u64,k:u64) -> u64 {
-    let list = (0..n).filter(|&x| x % k == n % k).collect::<Vec<_>>();
-    list[list.len()-1]
+fn find_mod(n: u128,k:u128) -> u128 {
+    let mut return_num:u128 = 0;
+    for i in 0..n{
+        if i%k == n%k && i != 1{
+            return_num = i;
+            break;
+        }
+    }
+    return_num
 }
-fn large_pow (x:u128,y:u128)->u128{
-    println!("{},{}",x,y);
-    // let mut result:String = format!("{}",x);
-    // for i in 0..y-1{
-    //     result *= result as u128
-    // }
-    // println!("{}",result);
-    // 23
-    23
+fn find_mod_2 (n:u64,k:u64)->u64{
+    let mut return_num:u64 = 0;
+    for i in 0..n{
+        if i%k == n%k && i != 1{
+            return_num = i;
+            println!("{}",i);
+            break;
+        }
+    }
+    return_num
 }
+
 fn main() {
     let factor = genarate_random_prime_num();
     let public_key = generate_public_key(factor.0,factor.1);
@@ -95,7 +104,17 @@ fn main() {
     println!("publicKey:{},{}",public_key.n,public_key.e);
     println!("privateKey:{}",private_key.d);
     let secret_text:u64 = 7;
-    println!("{}",public_key.e);
-    let encoded_text:u64 = find_mod(secret_text.pow(public_key.e as u32), public_key.n as u64);
-    println!("{}",encoded_text);
+    let unit_m = BigUint::from(secret_text);
+    println!("{}",unit_m);
+    let unit_n = BigUint::from(public_key.n as u64);
+    let e_mess = public_key.e as usize;
+    let encode_text = pow(unit_m,e_mess)%&unit_n;
+    let decode_text = pow(encode_text,private_key.d as usize)%&unit_n;
+    println!("{}",decode_text);
+
+    // let base = 2;
+    // let ex = 200;
+    // let test = pow(base,ex);
+    // println!("{}",&test);
+    // let decode_text:u64 = 
 }
